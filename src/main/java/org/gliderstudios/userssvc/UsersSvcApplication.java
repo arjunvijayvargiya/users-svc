0e4a0a0c2dcb91439bfeadf9c3540c8dc1977dd7
arjunvijayvargiya <arjun.vijay20@gmail.com>
2019-10-09 23:40:44 +0530

    users service initial commit

---
+++ src/main/java/org/gliderstudios/userssvc/UsersSvcApplication.java
@@ -0,0 +1,13 @@
+package org.gliderstudios.userssvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersSvcApplication.class, args);
+	}
+
+}
