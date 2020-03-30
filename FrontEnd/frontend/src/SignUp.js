b324b1bad904ade8df2b24ebb618d6148769aeac
arjunvijayvargiya <arjun.vijay20@gmail.com>
2020-03-30 18:50:02 +0530

    Added Signup functionality

---
+++ FrontEnd/frontend/src/SignUp.js
@@ -0,0 +1,166 @@
+import React, { Component } from "react";
+import AppNav from "./AppNav";
+import "./App.css";
+import "./Login.css";
+import { Redirect } from "react-router";
+import { Col, Button, Label, Input, Form, FormGroup } from "reactstrap";
+import { withRouter } from "react-router";
+import axios from "axios";
+import { Container } from "react-bootstrap";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "",
+      password: "",
+      email: "",
+      phone: "",
+      redirect: false,
+      errorLogin: false,
+      errorReason: ""
+    };
+
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePhone = this.handleChangePhone.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeUsername(event) {
+    this.setState({ userName: event.target.value });
+  }
+
+  handleChangePassword(event) {
+    this.setState({ password: event.target.value });
+  }
+
+  handleChangeEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  handleChangePhone(value) {
+    this.setState({ phone:  value});
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const requestObj = {
+      userName: this.state.userName,
+      password: this.state.password,
+      email: this.state.email,
+      phone: this.state.phone
+    };
+    const jsStr = JSON.stringify(requestObj);
+    console.log(jsStr);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8282/api/v1/users",
+        jsStr,
+        { headers: { "Content-Type": "application/json" } }
+      );
+      // Success 🎉
+      console.log("SignUp successful");
+      this.setState({ redirect: true });
+    } catch (error) {
+      console.log("ERROR %s", JSON.stringify(error.response.data.response));
+      this.setState({ errorLogin: true });
+      this.setState({ errorReason: error.response.data.response });
+    }
+  }
+
+  async componentDidMount() {}
+
+  render() {
+    let errorText = <div></div>;
+
+    const { history } = this.props;
+
+    if (this.state.redirect) {
+      history.push("/login");
+      return (
+        <Redirect
+          to={{
+            pathname: "/login"
+          }}
+        />
+      );
+    }
+
+    if (this.state.errorLogin) {
+      errorText = <div className="errorText">{this.state.errorReason}</div>;
+    }
+    return (
+      <div>
+        <AppNav title="Expense Tracker" />
+        <Container>
+          <div className="spacer"></div>
+          <Form onSubmit={this.handleSubmit}>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="userName">Username</Label>
+                <Input
+                  type="text"
+                  name="text"
+                  id="userName"
+                  placeholder="type your username"
+                  value={this.state.userName}
+                  onChange={this.handleChangeUsername}
+                  required
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="password">Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="type your password"
+                  value={this.state.password}
+                  onChange={this.handleChangePassword}
+                  required
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="email">Email</Label>
+                <Input
+                  type="email"
+                  name="Email"
+                  id="email"
+                  placeholder="type your email"
+                  value={this.state.email}
+                  onChange={this.handleChangeEmail}
+                  required
+                />
+              </FormGroup>
+
+              <FormGroup>
+                <Label for="phone">Phone</Label>
+                <PhoneInput
+                  placeholder="Enter phone number"
+                  value={this.state.phone}
+                  onChange={this.handleChangePhone}
+                />
+              </FormGroup>
+
+              <div className="horizontalSignup">
+                <Button outline color="success" type="submit">
+                  SignUp
+                </Button>{" "}
+              </div>
+              {errorText}
+            </Col>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default withRouter(SignUp);
